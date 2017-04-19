9c6effc87970ca96ac1f6f7edfc0c3d3efd59444
Heathcliff <dennis.nathan@gmail.com>
2017-04-19 13:46:48 -0600

    Attempting to fix sideNav issue

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,12 @@
-//Use window.load for JS when using Materialize, document.ready doesn't work. 
+//Use window.load for JS when using Materialize, document.ready doesn't work.
 $(window).load(function() {
   console.log('bippety boppety boo');
   Materialize.updateTextFields();
-  $(".button-collapse").sideNav();
+  $('.button-collapse').sideNav({
+      menuWidth: 300,
+      edge: 'left',
+      closeOnClick: true,
+      draggable: false
+    }
+  );
 });
