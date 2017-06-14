7f55e0ace67b23a55222aabc39a512f38dcab5bc
Heathcliff <dennis.nathan@gmail.com>
2017-06-14 13:54:03 -0600

    Fixed issue with createNewUser function in main controller, can now create users

--- src/js/components/main/main.controller.js
+++ src/js/components/main/main.controller.js
@@ -6,14 +6,35 @@
     .module('jamesCoffeeApp.components.main', [])
     .controller('mainController', mainController);
 
-  mainController.$inject = ['$scope', '$mdSidenav'];
+  mainController.$inject = ['$scope', '$mdSidenav', '$http'];
 
-  function mainController($scope, $mdSidenav) {
+  function mainController($scope, $mdSidenav, $http) {
     /*jshint validthis: true */
 
     $scope.openLeftMenu = function() {
       $mdSidenav('left').toggle();
     };
+
+    $scope.firstName = '';
+    $scope.lastName = '';
+    $scope.email = '';
+    $scope.password = '';
+
+    $scope.createNewUser = function(firstName, lastName, email, password) {
+      console.log('something');
+      var userObject = {
+        firstName: firstName,
+        lastName: lastName,
+        email: email,
+        password: password
+      }
+      console.log(userObject);
+      $http({
+        url: 'https://quiet-sea-23927.herokuapp.com/newUser',
+        method: 'POST',
+        data: userObject
+      })
+    }
   }
 
 })();
